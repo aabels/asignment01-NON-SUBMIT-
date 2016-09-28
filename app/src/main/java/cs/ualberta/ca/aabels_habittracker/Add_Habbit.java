abb03e88b6fbcd75bf23d9b6cb4f6344e5b77090
aabels <aabels@ualberta.ca>
2016-09-27 20:19:25 -0600

    Added the Create UI Build

---
+++ app/src/main/java/cs/ualberta/ca/aabels_habittracker/Add_Habbit.java
@@ -0,0 +1,13 @@
+package cs.ualberta.ca.aabels_habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add_Habbit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__habbit);
+    }
+}
