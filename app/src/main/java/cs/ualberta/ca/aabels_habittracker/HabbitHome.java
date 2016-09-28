70b00d74585f0b9449a54e1936ffd9d5a4dace91
aabels <aabels@ualberta.ca>
2016-09-27 19:36:00 -0600

    This is first commit with home UI build

---
+++ app/src/main/java/cs/ualberta/ca/aabels_habittracker/HabbitHome.java
@@ -0,0 +1,13 @@
+package cs.ualberta.ca.aabels_habittracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HabbitHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habbit_home);
+    }
+}
