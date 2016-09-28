26764f5d0192c4617a18ba623c991a9d4ac74cca
aabels <aabels@ualberta.ca>
2016-09-27 21:27:56 -0600

    Added the navigation to the create habit page:D

--- app/src/main/java/cs/ualberta/ca/aabels_habittracker/HabbitHome.java
+++ app/src/main/java/cs/ualberta/ca/aabels_habittracker/HabbitHome.java
@@ -1,7 +1,10 @@
 package cs.ualberta.ca.aabels_habittracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HabbitHome extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class HabbitHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habbit_home);
+
+        Button addButton = (Button)findViewById(R.id.Add_Habit_Button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),Add_Habbit.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
