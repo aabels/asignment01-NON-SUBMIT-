c81f4559cd013f4ed64b4186014ef011420d65ee
aabels <aabels@ualberta.ca>
2016-10-02 20:34:28 -0600

    changed my history

--- app/src/main/java/cs/ualberta/ca/aabels_habittracker/HabitHistory.java
+++ app/src/main/java/cs/ualberta/ca/aabels_habittracker/HabitHistory.java
@@ -2,6 +2,8 @@ package cs.ualberta.ca.aabels_habittracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -13,6 +15,7 @@ public class HabitHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_history);
+        Toast.makeText(HabitHistory.this, "Under Construction...", Toast.LENGTH_SHORT).show();
         HabitListManager.initHabitManager(this.getApplicationContext());
         Collection<Habit> habits = HabitListController.getHabitList().getHabits();
         final ArrayList list = new ArrayList<Habit>(habits);
